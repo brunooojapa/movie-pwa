2598bb476b00e2409daae38a8c85c07498e098d6
brunooojapa <brunooojapa@gmail.com>
2018-10-27 22:08:31 -0300

    Updates

---
+++ precache-manifest.5451057ddb3945936b0f1e6c9bbbaa8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cdbba73d66edb7f82b1",
+    "url": "/movie-pwa/static/js/runtime~main.7cdbba73.js"
+  },
+  {
+    "revision": "ecbcfe90d73635b1d864",
+    "url": "/movie-pwa/static/js/main.ecbcfe90.chunk.js"
+  },
+  {
+    "revision": "e830c0910a7ee5c6198b",
+    "url": "/movie-pwa/static/js/1.e830c091.chunk.js"
+  },
+  {
+    "revision": "ecbcfe90d73635b1d864",
+    "url": "/movie-pwa/static/css/main.7ff76a69.chunk.css"
+  },
+  {
+    "revision": "ae5b2ed32c39c3ada32e17ca79d80532",
+    "url": "/movie-pwa/index.html"
+  }
+];
